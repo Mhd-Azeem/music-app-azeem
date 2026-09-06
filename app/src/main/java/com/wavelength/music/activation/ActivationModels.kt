2e60d6c09999c2b66f5e7afcd2d912e60a050598
Mhd-Azeem <azeemzahira111@gmail.com>
2026-09-06 12:55:45 +0530

    Add activation models

---
+++ app/src/main/java/com/wavelength/music/activation/ActivationModels.kt
@@ -0,0 +1,26 @@
+package com.wavelength.music.activation
+
+import com.squareup.moshi.JsonClass
+
+enum class ActivationStatus {
+    NOT_ACTIVATED,
+    PENDING,
+    ACTIVE,
+    EXPIRED,
+    REJECTED,
+    REVOKED
+}
+
+@JsonClass(generateAdapter = true)
+data class ActivationRecord(
+    val email: String = "",
+    val status: ActivationStatus = ActivationStatus.NOT_ACTIVATED,
+    val requestedAt: Long? = null,
+    val approvedAt: Long? = null,
+    val activationStartDate: Long? = null,
+    val expirationDate: Long? = null,
+    val durationDays: Int? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class ActivationRequestBody(val email: String)
