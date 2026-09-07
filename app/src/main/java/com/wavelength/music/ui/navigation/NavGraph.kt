cecfb6073a81bc9c9f33c13578139240ec3748c6
Mhd-Azeem <azeemzahira111@gmail.com>
2026-09-07 21:31:38 +0530

    Fix activation settings button build

--- app/src/main/java/com/wavelength/music/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/wavelength/music/ui/navigation/NavGraph.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
