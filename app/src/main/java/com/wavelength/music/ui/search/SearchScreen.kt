7dddb2b8fce2446a3c197e606814b41d274d1fc2
Mhd-Azeem <azeemzahira111@gmail.com>
2026-09-07 18:50:38 +0530

    Fix Compose weight import build error

--- app/src/main/java/com/wavelength/music/ui/search/SearchScreen.kt
+++ app/src/main/java/com/wavelength/music/ui/search/SearchScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
